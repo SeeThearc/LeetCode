96c3406308177258c4934ecb6de8fab7deb52f11
SeeThearc <ayushagrawal2334@gmail.com>
2025-04-03 15:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (79.43%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,6 +3,9 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
+        //use firstrow and firstcolumn to mark if row or column contains 0
+        // we use first row for row and first column for column 
+        // as matrix[0][0]appear twice so for column we create new variable col0 and we use    matrix[0][0 for row
         int col0=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -17,6 +20,7 @@ public:
                 }
             }
         }
+        // mark from 1,1 to n,m
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j]!=0){
@@ -26,6 +30,7 @@ public:
                 }
             }
         }
+        //check for first row and column
         if(matrix[0][0]==0){
             for(int j=0;j<m;j++){
                 matrix[0][j]=0;
