84fa66c24a6f313a3d9112e772e4ab4276ff77ec
SeeThearc <ayushagrawal2334@gmail.com>
2025-08-28 13:52:16 +0530

    Time: 15 ms (29.84%) | Memory: 45.4 MB (28.19%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -3,24 +3,28 @@ public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        unordered_map<int,vector<int>>mpp;
+        unordered_map<int,priority_queue<int,vector<int>>>maxheap;
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>>minheap;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                mpp[i-j].push_back(grid[i][j]);
-            }
-        }
-        for(auto &it:mpp){
-            if(it.first>=0){
-                sort(it.second.begin(),it.second.end(),greater<int>());
-            }
-            else{
-                sort(it.second.begin(),it.second.end());
+                if(i-j<0){
+                    minheap[i-j].push(grid[i][j]);
+                }
+                else{
+                    maxheap[i-j].push(grid[i][j]);
+                }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                grid[i][j] = mpp[i-j].front();
-                mpp[i-j].erase(mpp[i-j].begin());
+                if(i-j<0){
+                    grid[i][j] = minheap[i-j].top();
+                    minheap[i-j].pop();
+                }
+                else{
+                    grid[i][j] = maxheap[i-j].top();
+                    maxheap[i-j].pop();
+                }
             }
         }
         return grid;
