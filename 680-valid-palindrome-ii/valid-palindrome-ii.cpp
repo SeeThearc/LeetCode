dd4d0f9a7750332a40647aa38211705fffdfa5f5
SeeThearc <ayushagrawal2334@gmail.com>
2025-09-12 02:44:56 +0530

    Time: 50 ms (5.81%) | Memory: 39 MB (5.38%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool palinrange(string s,int left,int right){
+        while(left<right){
+            if(s[left]!=s[right])return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        stringstream ss(s);
+        char c;
+        vector<char>lett;
+        while(ss >> c){
+            lett.push_back(c);
+        }
+        int left=0,right=lett.size()-1;
+        while(left<right){
+            if(lett[left]==lett[right]){
+                left++;
+                right--;
+            }
+            else{
+                return palinrange(s,left+1,right) || palinrange(s,left,right-1);
+            }
+        }
+        return true;
+    }
+};
