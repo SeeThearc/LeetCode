709019d9d4fc8b2ce26e89b6beac968cf58cba16
SeeThearc <ayushagrawal2334@gmail.com>
2025-09-12 03:51:44 +0530

    Time: 34 ms (5.18%) | Memory: 19.5 MB (58.24%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,35 +3,34 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = prerequisites.size();
         vector<vector<int>>adjLis(numCourses);
-        vector<int>inDegree(numCourses);
+        vector<int>indegree(numCourses,0);
         for(int i=0;i<n;i++){
             adjLis[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            inDegree[prerequisites[i][1]]++;
+            indegree[prerequisites[i][1]]++;
         }
         vector<int>vis(numCourses,0);
         queue<int>q;
         for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0){
+            if(indegree[i]==0){
                 q.push(i);
                 vis[i]=1;
             }
         }
         vector<int>ans;
         while(!q.empty()){
-            int temp = q.front();
+            auto it = q.front();
             q.pop();
-            ans.push_back(temp);
-            for(auto it:adjLis[temp]){
-                inDegree[it]--;
+            ans.push_back(it);
+            for(int i=0;i<adjLis[it].size();i++){
+                indegree[adjLis[it][i]]--;
             }
             for(int i=0;i<numCourses;i++){
-                if(!vis[i] && inDegree[i]==0){
+                if(!vis[i] && indegree[i]==0){
                     q.push(i);
                     vis[i]=1;
                 }
             }
         }
-        if(ans.size()==numCourses)return true;
-        return false;
+        return ans.size()==numCourses;
     }
 };
