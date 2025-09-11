0a1fd4b486020461e32aca77e912e7f835086291
SeeThearc <ayushagrawal2334@gmail.com>
2025-09-11 23:21:42 +0530

    Time: 51 ms (5.22%) | Memory: 16.3 MB (66.53%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    bool isVowel(char c){
+        int ans = string("AEIOUaeiou").find(c);
+        if(ans>=0)return true;
+        return false;
+    }
     string sortVowels(string s) {
-        priority_queue<char,vector<char>,greater<char>>pq;
-        vector<int>ind;
-        int n = s.size();
-        regex vowel("[aeiouAEIOU]");
-        for(int i=0;i<n;i++){
-            if(regex_match(string(1,s[i]),vowel)){
-                pq.push(s[i]);
-                ind.push_back(i);
+        vector<char>vowel;
+        for(char c:s){
+            if(isVowel(c)){
+                vowel.push_back(c);
             }
         }
-        if(ind.size()==0)return s;
-        int k=0,i=0;
-        while(i<n && k<ind.size()){
-            if(i==ind[k]){
-                char ch = pq.top();
-                s[i]=ch;
-                k++;
-                pq.pop();
+        sort(vowel.begin(),vowel.end());
+        int i=0;
+        for(char &c:s){
+            if(isVowel(c)){
+                c = vowel[i];
+                i++;
             }
-            i++;
+            if(i==vowel.size())break;
         }
         return s;
     }
