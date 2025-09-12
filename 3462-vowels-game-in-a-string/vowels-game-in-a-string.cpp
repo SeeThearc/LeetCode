3c33a4cd42b712abbb8dec80d86126446f1cac14
SeeThearc <ayushagrawal2334@gmail.com>
2025-09-13 01:00:30 +0530

    Time: 12 ms (28.92%) | Memory: 19.3 MB (16.23%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool doesAliceWin(string s) {
         string vowel = "aeiouAEIOU";
-        int co=0;
         for(char &c:s){
-            if(vowel.find(c)!=string::npos)co++;
-            if(co>0)return true;
+            if(vowel.find(c)!=string::npos)return true;
         }
         return false;
     }
