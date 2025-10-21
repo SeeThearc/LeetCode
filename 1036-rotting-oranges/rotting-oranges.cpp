f4eb02630a6760a4b377a94bac58772e47852f74
SeeThearc <ayushagrawal2334@gmail.com>
2025-10-22 00:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (74.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m =grid.size();
+        int m = grid.size();
         int n = grid[0].size();
         queue<pair<int,int>>q;
-        int c=0;
+        int dirx[] = {0,0,1,-1};
+        int diry[] = {1,-1,0,0};
+        int total=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                if(grid[i][j]!=0)c++;
+                if(grid[i][j]!=0)total++;
             }
         }
-        int ans=0;
-        int dirx[] = {0,0,1,-1};
-        int diry[] = {1,-1,0,0};
-        int cf=0;
+        if(total==0)return 0;
+        int rot=0;
+        int min=-1;
         while(!q.empty()){
-            bool found=false;
-            int s = q.size();
-            cf+=s;
-            for(int j=0;j<s;j++){
-                auto temp = q.front();
-                int x = temp.first;
-                int y = temp.second;
+            int size = q.size();
+            rot+=size;
+            min++;
+            for(int i=0;i<size;i++){
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
-                for(int i=0;i<4;i++){
-                    int nx = x+dirx[i];
-                    int ny = y+diry[i];
+                for(int k=0;k<4;k++){
+                    int nx = x + dirx[k];
+                    int ny = y + diry[k];
                     if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1){
-                        found=true;
                         grid[nx][ny]=2;
                         q.push({nx,ny});
                     }
                 }
             }
-            if(found)ans++;
-            found=false;
         }
-        int fin;
-        if(c==cf?fin=ans:fin=-1);
-        return fin;
+        int ans=-1;
+        if(rot==total?ans=min:ans=-1);
+        return ans;
     }
 };
