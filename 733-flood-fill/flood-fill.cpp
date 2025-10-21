cdb2381a8d73f1a443efbf956c7e8cc2ecc6c902
SeeThearc <ayushagrawal2334@gmail.com>
2025-10-22 00:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (16.87%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)return image;
-        int org = image[sr][sc];
         int m = image.size();
         int n = image[0].size();
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        image[sr][sc]=color;
+        int org = image[sr][sc];
         int dirx[] = {0,0,1,-1};
         int diry[] = {1,-1,0,0};
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        visited[sr][sc]=1;
+        image[sr][sc]=color;
         while(!q.empty()){
-            auto temp = q.front();
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-            int x = temp.first;
-            int y = temp.second;
             for(int i=0;i<4;i++){
                 int nx = x + dirx[i];
                 int ny = y + diry[i];
-                if(nx>=0 && nx<m && ny>=0 && ny<n && image[nx][ny]==org){
-                    image[nx][ny]=color;
+                if(nx>=0 && nx<m && ny>=0 && ny<n && !visited[nx][ny] &&image[nx][ny]==org){
                     q.push({nx,ny});
+                    image[nx][ny]=color;
+                    visited[nx][ny]=1;
                 }
             }
         }
