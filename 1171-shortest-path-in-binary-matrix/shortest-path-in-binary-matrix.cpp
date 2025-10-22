59776ed6cc028d73d1f8b6612fcb76e68f2d2cba
SeeThearc <ayushagrawal2334@gmail.com>
2025-10-22 13:55:52 +0530

    Time: 19 ms (57.14%) | Memory: 23.5 MB (91.69%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,24 +4,22 @@ public:
         int n = grid.size();
         if(grid[0][0]==1 || grid[n-1][n-1]==1)return -1;
         queue<pair<int,int>>q;
+        vector<pair<int,int>>dir = {
+            {1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}
+        };
         q.push({0,0});
         grid[0][0]=1;
-        vector<pair<int,int>>dir = {
-            {1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}
-        };
         while(!q.empty()){
-            auto temp = q.front();
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-            int x = temp.first;
-            int y = temp.second;
-            int dist = grid[x][y];
-            if(x==n-1 && y==n-1)return dist;
+            if(x==n-1 && y==n-1)return grid[x][y];
             for(auto [dx,dy] : dir){
                 int nx = x + dx;
                 int ny = y + dy;
                 if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0){
+                    grid[nx][ny] = 1 + grid[x][y];
                     q.push({nx,ny});
-                    grid[nx][ny] = 1 + dist;
                 }
             }
         }
