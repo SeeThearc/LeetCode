090470da43f2afb84d50fa910970781202166139
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-02-22 16:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.36%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -9,16 +9,19 @@ public:
                 break;
             }
         }
+        //no breaking point ie reverse the array
         if(i==-1){
             reverse(nums.begin(),nums.end());
             return;
         }
+        //find first point which is greater than breaking point num and then swap
         for(int j=n-1;j>=0;j--){
             if(nums[i]<nums[j]){
                 swap(nums[i],nums[j]);
                 break;
             }
         }
+        //reverse the part after breaking point
         reverse(nums.begin()+i+1,nums.end());
         return;
     }
