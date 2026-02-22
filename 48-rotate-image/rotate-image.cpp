9f7b469e5e0f41b83a6f10796eb3f18cf06327f9
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-02-22 18:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (69.51%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++){
+        // to rotate clock answer is transpose and reverse
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<m;i++){
             for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++){
+        for(int i=0;i<m;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
