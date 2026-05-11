314ea1ce05215a1a5604f8c7f2ae4bc082905441
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-05-11 15:35:00 +0530

    Time: 3 ms (57.62%) | Memory: 15.4 MB (54.47%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -2,18 +2,8 @@ class Solution {
 public:
     void finder(vector<int>&dig,int n){
         string s = to_string(n);
-        bool found=false;
-        if(s[0]=='0')found=true;
-        if(found)dig.push_back(0);
-        vector<int>digtemp;
-        while(n>0){
-            int digi = n%10;
-            digtemp.push_back(digi);
-            n/=10;
-        }
-        reverse(digtemp.begin(),digtemp.end());
-        for(int x:digtemp){
-            dig.push_back(x);
+        for(int i=0;i<s.size();i++){
+            dig.push_back(s[i]-'0');
         }
     }
     vector<int> separateDigits(vector<int>& nums) {
