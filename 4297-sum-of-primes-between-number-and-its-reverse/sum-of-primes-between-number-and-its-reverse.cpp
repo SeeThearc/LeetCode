550dc10ca3346ebb8c49f517b0653f7df25d0d2f
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-05-14 23:05:11 +0530

    Time: 4 ms (48.87%) | Memory: 9.9 MB (13.81%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
@@ -8,7 +8,7 @@ public:
         int high = max(n,rev);
         vector<bool>pri(high+1,true);
         pri[0]=pri[1]=false;
-        for(int i=2;i<=high;i++){
+        for(int i=2;i*i<=high;i++){
             if(pri[i]){
                 for(int j=i*i;j<=high;j+=i){
                     pri[j]=false;
