6e5d4d3d0fdab708bb30034534dc866508102c8a
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-05-14 23:04:24 +0530

    Time: 2 ms (73.54%) | Memory: 9.8 MB (13.81%) - LeetSync

---
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sumOfPrimesInRange(int n) {
+        string s = to_string(n);
+        reverse(s.begin(),s.end());
+        int rev = stoi(s);
+        int low = min(n,rev);
+        int high = max(n,rev);
+        vector<bool>pri(high+1,true);
+        pri[0]=pri[1]=false;
+        for(int i=2;i<=high;i++){
+            if(pri[i]){
+                for(int j=i*i;j<=high;j+=i){
+                    pri[j]=false;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=low;i<=high;i++){
+            if(pri[i]){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
