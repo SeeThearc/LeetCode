8c10867ae12237a311953528969733be5c837803
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-03 23:55:39 +0530

    Time: 2 ms (20.90%) | Memory: 9.3 MB (51.30%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>&dp){
-        if(i==0 && j==0)return 1;
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        return dp[i][j] = helper(i-1,j,dp) + helper(i,j-1,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        vector<vector<int>>grid(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    grid[i][j] = 1;
+                    continue;
+                }
+                int sum1=0,sum2=0;
+                if(i>0){
+                    sum1 = grid[i-1][j];
+                }
+                if(j>0){
+                    sum2 = grid[i][j-1];
+                }
+                grid[i][j] = sum1+sum2;
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
