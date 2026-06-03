2015d6e0cac69e5ec05b6ba56e96fdae0a3236c9
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-04 00:31:59 +0530

    Time: 3 ms (28.45%) | Memory: 16.2 MB (58.31%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
-        if(i==0 && j==0)return grid[i][j];
-        if(i<0 || j<0)return 1e9;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int up = grid[i][j] + helper(i-1,j,grid,dp);
-        int left = grid[i][j] + helper(i,j-1,grid,dp);
-        return dp[i][j] = min(up,left);
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,grid,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int sum1=INT_MAX,sum2=INT_MAX;
+                if(i>0){
+                    sum1 = dp[i-1][j];
+                }
+                if(j>0){
+                    sum2 = dp[i][j-1];
+                }
+                dp[i][j] = min(sum1,sum2);
+                dp[i][j]+=grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
