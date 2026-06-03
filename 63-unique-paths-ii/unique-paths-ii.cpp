340d0f03963ce362b525d8ae7ae46175e006be76
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-04 00:04:37 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.36%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp){
-        if(i<0 || j<0)return 0;
-        if(grid[i][j]==1)return 0;
-        if(i==0 && j==0)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        return dp[i][j] = helper(i-1,j,grid,dp) + helper(i,j-1,grid,dp);
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,obstacleGrid,dp);
+        vector<vector<int>>grid(m,vector<int>(n,0));
+        if(obstacleGrid[0][0]==1)return 0;
+        if(m==1 && n==1)return 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    grid[i][j]=1;
+                    continue;
+                }
+                int sum1=0,sum2=0;
+                if(obstacleGrid[i][j]==0){
+                    if(i>0 && obstacleGrid[i-1][j]!=1){
+                        sum1 = grid[i-1][j];
+                    }
+                    if(j>0 && obstacleGrid[i][j-1]!=1){
+                        sum2 = grid[i][j-1];
+                    }
+                    grid[i][j] = sum1+sum2;
+                }
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
