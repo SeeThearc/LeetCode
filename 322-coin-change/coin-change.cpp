7a194bc00c83db4abda7c9672d899677f2536bbd
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-09 02:24:54 +0530

    Time: 47 ms (31.78%) | Memory: 49.1 MB (29.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
+    int find(int ind,int amount,vector<int>&coins,vector<vector<int>>&dp){
+        if(amount<0)return 1e9;
+        if(amount==0)return 0;
+        if(ind==0){
+            if(amount%coins[0]==0)return amount/coins[0];
+            else return 1e9;
+        }
+        if(dp[ind][amount]!=INT_MAX)return dp[ind][amount];
+        int notpick = find(ind-1,amount,coins,dp);
+        int pick = 1 + find(ind,amount-coins[ind],coins,dp);
+        return dp[ind][amount] = min(pick,notpick);
+    }
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        int mod = 1e9 + 7;
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
-            }
-            else{
-                dp[0][i]=1e9;
-            }
+        if(amount==0)return 0;
+        int n = coins.size();
+        if(n==1){
+            if(amount%coins[0]==0)return amount/coins[0];
+            else return -1;
         }
-        for(int ind =1;ind<n;ind++){
-            for(int tar=0;tar<=amount;tar++){
-                int notTaken = dp[ind-1][tar];
-                int taken = 1e9;
-                if(coins[ind]<=tar){
-                    taken = 1 + dp[ind][tar-coins[ind]];
-                }
-                dp[ind][tar] = min(notTaken,taken);
-            }
-        }
-        int ans =  dp[n-1][amount];
-        if(ans >=1e9){
-            return -1;
-        }
+        vector<vector<int>>dp(n,vector<int>(amount+1,INT_MAX));
+        int ans = find(n-1,amount,coins,dp);
+        if(ans==0 || ans>=1e9)return -1;
         return ans;
     }
 };
