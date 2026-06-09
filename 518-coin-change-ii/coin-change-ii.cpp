235cbcb85780087f569bb3bf8d3fc82f9b1acce7
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-10 01:22:32 +0530

    Time: 25 ms (67.89%) | Memory: 36.5 MB (53.72%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
+    int find(int ind,int amo,vector<int>&coins,vector<vector<int>>&dp){
+        if(amo<0)return 0;
+        if(ind==0){
+            return (amo%coins[0]==0);
+        }
+        if(dp[ind][amo]!=-1)return dp[ind][amo];
+        int notpick = find(ind-1,amo,coins,dp);
+        int pick = find(ind,amo-coins[ind],coins,dp);
+        return dp[ind][amo] = pick + notpick;
+    }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        int mod = 1e9;
-        vector<vector<unsigned long long>>dp(n,vector<unsigned long long>(amount+1,0));
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=1;
-            }
-        }
-        for(int ind=1;ind<n;ind++){
-            for(int tar=0;tar<=amount;tar++){
-                long long notTaken = dp[ind-1][tar];
-                long long taken = 0;
-                if(coins[ind]<=tar){
-                    taken = dp[ind][tar-coins[ind]];
-                }
-                dp[ind][tar] = (unsigned long long)notTaken + taken; 
-            }
-        }
-        return (int)dp[n-1][amount];
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return find(n-1,amount,coins,dp);
     }
 };
