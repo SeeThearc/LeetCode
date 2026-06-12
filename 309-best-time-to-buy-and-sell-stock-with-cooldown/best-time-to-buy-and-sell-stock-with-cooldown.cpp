59d54122ffc6bc531c56ed7115d5fcee429acd08
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-13 02:23:26 +0530

    Time: 4 ms (19.48%) | Memory: 16.4 MB (22.71%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,9 +3,9 @@ public:
     int find(int ind,int canbuy,int cool,int n,vector<int>&prices,vector<vector<int>>&dp){
         if(ind>=n)return 0;
         if(dp[ind][canbuy]!=-1)return dp[ind][canbuy];
-        if(cool){
-            return find(ind+1,canbuy,0,n,prices,dp);
-        }
+        // if(cool){
+        //     return find(ind+1,canbuy,0,n,prices,dp);
+        // }
         int profit=0;
         if(canbuy){
             profit = max(0+find(ind+1,1,0,n,prices,dp),-prices[ind]+find(ind+1,0,0,n,prices,dp));
