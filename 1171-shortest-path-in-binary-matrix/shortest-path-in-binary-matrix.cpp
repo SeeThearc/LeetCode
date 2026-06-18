05b36def753227f54f7dc1e7bf38e0e2a4497e00
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-06-18 06:23:21 +0530

    Time: 19 ms (52.82%) | Memory: 23.3 MB (98.81%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1)return -1;
         queue<pair<int,int>>q;
-        vector<pair<int,int>>dir = {
-            {1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}
-        };
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0)return -1;
+        vector<pair<int,int>>dir = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
         q.push({0,0});
-        grid[0][0]=1;
         while(!q.empty()){
             int x = q.front().first;
             int y = q.front().second;
             q.pop();
-            if(x==n-1 && y==n-1)return grid[x][y];
-            for(auto [dx,dy] : dir){
-                int nx = x + dx;
-                int ny = y + dy;
+            if(x==n-1 && y==n-1)return 1+grid[x][y];
+            for(auto [dx,dy]:dir){
+                int nx = x+dx;
+                int ny = y+dy;
                 if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0){
-                    grid[nx][ny] = 1 + grid[x][y];
+                    grid[nx][ny] = 1+grid[x][y];
                     q.push({nx,ny});
                 }
             }
