e3ff3264ac063e052b125fe7c3c309bc055bbf7f
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-07-20 19:19:17 +0530

    Time: 23 ms (40.10%) | Memory: 38.7 MB (38.10%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+        int m = mat.size();
+        int n = mat[0].size();
         queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
                     vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
-        vector<vector<int>>dis(n,vector<int>(m));
-        int dirx[] = {0,0,1,-1};
-        int diry[] = {1,-1,0,0};
+        vector<pair<int,int>>dir = {{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.empty()){
-            auto temp = q.front();
+            auto it = q.front();
             q.pop();
-            int x = temp.first.first;
-            int y = temp.first.second;
-            int step = temp.second;
-            dis[x][y] = step;
+            int x = it.first.first;
+            int y = it.first.second;
+            int step = it.second;
+            mat[x][y] = step;
             for(int i=0;i<4;i++){
-                int nx = x + dirx[i];
-                int ny = y + diry[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny]){
-                    q.push({{nx,ny},step+1});
+                int nx = x + dir[i].first;
+                int ny = y + dir[i].second;
+                if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny]){
                     vis[nx][ny]=1;
+                    q.push({{nx,ny},step+1});
                 }
             }
         }
-        return dis;
+        return mat;
     }
 };
