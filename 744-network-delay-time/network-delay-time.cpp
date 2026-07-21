624ef0a3266720ad7314bfbd0a28a5661a3ec9ee
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-07-21 19:05:15 +0530

    Time: 87 ms (43.89%) | Memory: 44.5 MB (53.32%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adjLis(n+1);
-        for(int i=0;i<times.size();i++){
-            adjLis[times[i][0]].push_back({times[i][1],times[i][2]});
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(int i=0;i<(int)times.size();i++){
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
         }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-        vector<int>dist(n+1,1e9);
+        vector<int>dist(n+1,INT_MAX);
         dist[k]=0;
         pq.push({0,k});
         while(!pq.empty()){
-            auto temp = pq.top();
+            auto m = pq.top();
             pq.pop();
-            int time = temp.first;
-            int node = temp.second;
-            if(time>dist[node])continue;
-            for(auto it:adjLis[node]){
+            int node = m.second;
+            int we = m.first;
+            if(we>dist[node])continue;
+            for(auto it:adj[node]){
                 int v = it.first;
                 int w = it.second;
                 if(dist[node]+w<dist[v]){
@@ -24,11 +24,13 @@ public:
                 }
             }
         }
-        int maxi=INT_MIN;
-        for(int i=1;i<=n;i++){
-            maxi = max(maxi,dist[i]);
+        int ans=-1;
+        int c=0;
+        for(int i=1;i<(int)dist.size();i++){
+            if(dist[i]==INT_MAX)continue;
+            ans=max(ans,dist[i]);
+            c++;
         }
-        if(maxi==1e9)return -1;
-        return maxi;
+        return (c==n?ans:-1);
     }
 };
