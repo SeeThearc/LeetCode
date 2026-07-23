ee542477da333b42eb66e6046999fe946071ef7b
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-07-24 02:56:06 +0530

    Time: 35 ms (33.81%) | Memory: 27.1 MB (53.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
+    int find(int i,int j,int n1,int n2,vector<vector<int>>&dp,string &s,string &t){
+        if(j==t.size())return 1;
+        if(i==s.size())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int pick = 0 ,notpick=0;
+        if(s[i]==t[j]){
+            pick = find(i+1,j+1,n1,n2,dp,s,t);
+        }
+        notpick = find(i+1,j,n1,n2,dp,s,t);
+        return dp[i][j] = pick+notpick;
+    }
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        int mod=1e9+7;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int ind1=1;ind1<=n;ind1++){
-            for(int ind2=1;ind2<=m;ind2++){
-                if(s[ind1-1]==t[ind2-1]){
-                    dp[ind1][ind2] = (dp[ind1-1][ind2-1]+dp[ind1-1][ind2])%mod;
-                }
-                else{
-                    dp[ind1][ind2]= dp[ind1-1][ind2];
-                }
-            }
-        }
-        return dp[n][m];
+        int n1 = s.size();
+        int n2 = t.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return find(0,0,n1,n2,dp,s,t);
     }
 };
