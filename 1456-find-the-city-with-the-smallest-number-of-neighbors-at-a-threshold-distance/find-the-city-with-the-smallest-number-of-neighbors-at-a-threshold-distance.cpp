d15852d048e799ed254affe998784b1babbd031c
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-07-23 13:49:24 +0530

    Time: 39 ms (6.91%) | Memory: 19.4 MB (21.44%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int dji(int src,vector<vector<pair<int,int>>>&adjlis,int n,int thresh){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        pq.push({0,src});
+        while(!pq.empty()){
+            auto [we,node] = pq.top();
+            pq.pop();
+            if(we>thresh)continue;
+            for(int i=0;i<(int)adjlis[node].size();i++){
+                int v = adjlis[node][i].first;
+                int w = adjlis[node][i].second;
+                if(we+w<dist[v] && we+w<=thresh){
+                    dist[v] = we + w;
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(dist[i]!=INT_MAX)c++;
+        }
+        return c;
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        int ans=0;
+        int mincity = INT_MAX;
+        vector<vector<pair<int,int>>>adjlis(n);
+        for(int i=0;i<(int)edges.size();i++){
+            adjlis[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            adjlis[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+        }
+        for(int i=0;i<n;i++){
+            int c = dji(i,adjlis,n,distanceThreshold);
+            if(c<mincity){
+                mincity = c;
+                ans=i;
+            }
+            else if(c==mincity){
+                if(i>ans)ans=i;
+            }
+        }
+        return ans;
+    }
+};
