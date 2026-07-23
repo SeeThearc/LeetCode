be9bf8afc32e2a978973d530852360b1c622129a
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-07-24 02:16:54 +0530

    Time: 3 ms (15.67%) | Memory: 11.9 MB (5.30%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int find(int i,string s,vector<int>&dp,int n){
+        if(i>=n)return 1;
+        if(dp[i]!=-1)return dp[i];
+        int goone=0,gotwo=0;
+        if(s[i]=='0'){
+            find(i+1,s,dp,n);
+        }
+        else{
+            goone = find(i+1,s,dp,n);
+        }
+        if(i<n-1 && ((s[i]=='2' && s[i+1]<'7') || (s[i]=='1'))){
+            gotwo = find(i+2,s,dp,n);
+        }
+        return dp[i] = goone+gotwo;
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.size(),-1);
+        int n = s.size();
+        return find(0,s,dp,n);
+    }
+};
