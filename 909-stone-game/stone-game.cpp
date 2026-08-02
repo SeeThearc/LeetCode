ba03fa8f2544430c22a599746f5ea032cb9ee852
Ayush Agrawal <ayushagrawal2334@gmail.com>
2026-08-03 03:27:03 +0530

    Time: 439 ms (5.13%) | Memory: 696.8 MB (5.02%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool find(int i,int j,vector<int>&piles,vector<vector<int>>&dp){
-        if(i==j)return 0;
+        if(i==j)return piles[i];
         if(dp[i][j]!=-1)return dp[i][j];
         int takeleft = piles[i] - find(i+1,j,piles,dp);
         int takeright = piles[i] - find(i,j-1,piles,dp);
@@ -12,6 +12,6 @@ public:
         int sum=0;
         for(int x:piles)sum+=x;
         vector<vector<int>>dp(n,vector<int>(sum,-1));
-        return find(0,n-1,piles,dp);
+        return find(0,n-1,piles,dp)>=0;
     }
 };
